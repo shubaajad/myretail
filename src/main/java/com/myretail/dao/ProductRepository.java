8739c3b1c94ad95b80f484762762b9e0eef17cae
Devanathan <sdevana@kih.kmart.com>
2017-03-31 13:34:59 -0500

    Modifying query param

--- src/main/java/com/myretail/dao/ProductRepository.java
+++ src/main/java/com/myretail/dao/ProductRepository.java
@@ -4,7 +4,7 @@
 package com.myretail.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,9 +12,10 @@ import org.springframework.stereotype.Repository;
  *
  */
 @Repository("ProductRepository")
-public interface ProductRepository extends MongoRepository<ProductDao, String> {
+public interface ProductRepository extends MongoRepository<ProductDao, Integer> {
 	
-	public ProductDao findByPid(@Param("id") int productId);
+	@Query("{ 'pid' : ?0 }")
+	public ProductDao findByPid(int productId);
 	
 	@SuppressWarnings("unchecked")
 	public ProductDao save(ProductDao productDao);
